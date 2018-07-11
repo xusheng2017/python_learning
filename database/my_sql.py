a1801b8a91633a3d4289b98fd6c7ea97aeafab68
xuxu <784826296@qq.com>
2018-07-11 23:02:55 +0800

    add database web

---
+++ database/my_sql.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+
+cursor = conn.cursor()
+
+cursor.execute('create table user(id varchar(20) primary key , name varchar(20))')
+
+cursor.execute('insert into user(id , name) values (\'1\' , \'lemon\')')
+cursor.execute('insert into user(id , name) values (\'2\' , \'zhangsan\')')
+cursor.execute('insert into user(id , name) values (\'3\' , \'lisi\')')
+cursor.execute('insert into user(id , name) values (\'4\' , \'wangwu\')')
+
+print(cursor.rowcount)
+
+cursor.close()
+
+conn.commit()
+
+conn.close()
